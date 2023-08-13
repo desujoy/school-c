f434cf60ad1a504682e1b5fa8e1ad14ee649e898
Sujoy De <sujoy.de345@gmail.com>
2023-08-13 19:04:25 +0000

    fix

--- GoalKicker.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    //return 0;
-    puts("Hello World!");
-    return 0;
-}
--- hello.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-main()
-{
-  printf("Hello World!");
-  getch();
-}
